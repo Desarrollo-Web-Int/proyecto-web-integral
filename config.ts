852b81e186855850178b57f9f4b48d8e15bf5562
MonseCastanon <monsecastanon023@gmail.com>
2026-05-28 19:52:26 -0600

    Confirguración de Base de datos

--- config.ts
+++ config.ts
@@ -1,2 +1,8 @@
 // Confirguración de la base de datos
 // Se configurara la base de datos
+export const DB_CONFIG = {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'my_database'
+};
